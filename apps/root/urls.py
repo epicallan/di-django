367a1e64e1134e9a3aa8e51ea87fe223d238cc83
epicallan <epicallan.al@gmail.com>
2016-03-29 15:23:19 +0300

    dockerised app

--- apps/root/urls.py
+++ apps/root/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 admin.autodiscover()
 
@@ -8,3 +10,4 @@ urlpatterns = patterns(
     url(r'^api/', include('spotlight.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()  # makes it possible for static files to work
